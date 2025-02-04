eee998bab94d7468cc489e3c6448c39cc23d2cc6
Shirayuki39 <lorddemecrius83@proton.me>
2025-02-04 22:36:50 +0800

    dolby: Remove duplicate ro.audio.monitorRotation
    * Already defined by Common
    
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>

--- setup.mk
+++ setup.mk
@@ -9,7 +9,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
     persist.vendor.audio_fx.current=dolby \
     ro.vendor.audio.dolby.dax.support=true \
     ro.vendor.dolby.dax.version=DAX3_3.7.0.8_r1 \
-    ro.audio.monitorrotation=true \
     ro.audio.monitorWindowRotation=true \
     vendor.audio.dolby.ds2.enabled=false \
     vendor.audio.dolby.ds2.hardbypass=false
